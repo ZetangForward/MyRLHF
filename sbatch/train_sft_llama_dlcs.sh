cd24c23ded084fdaee0654f25012394577fbe8f8
ZetangForward <zecheng.tang@foxmail.com>
2024-10-24 17:33:42 +0800

    dsa

---
+++ sbatch/train_sft_llama_dlcs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --job-name=train_llama_instruct    # 作业名
+#SBATCH --nodes=1                          # 使用一个节点
+#SBATCH --ntasks-per-node=1                # 每个节点的任务数
+#SBATCH --gres=gpu:8                       # 每个节点需要8个GPU
+#SBATCH --cpus-per-task=48                 # 分配给每个任务的CPU数目为该节点的CPU总数
+#SBATCH --mem=800G                         # 使用该节点上所有可用内存
+#SBATCH --time=infinite                    # 无限运行时间
+#SBATCH --output=sbatch/logs/zc_job_id-%J.out        # 标准输出重定向到job_id-<jobid>.out文件
+#SBATCH --error=sbatch/logs/zc_job_id-%J.err         # 标准错误重定向到job_id-<jobid>.err文件
+#SBATCH --partition=GPU_Monopolize2                    # 指定分区
+#SBATCH --exclusive                        # 独占申请的节点
+
+source activate zecheng
+
+cd /public/home/zecheng/workspace/zecheng/MyRLHF/
+bash scripts/train_sft_llama_dlcs.sh
+
--- scripts/train_sft_llama_dlcs.sh
+++ scripts/train_sft_llama_dlcs.sh
@@ -11,10 +11,10 @@ CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 deepspeed --include localhost:0,1,2,3,4,5,6
    --lora_rank 32 \
    --apply_chat_template \
    --pretrain '/public/home/zecheng/workspace/hf_models/Meta-Llama-3.1-8B-Instruct' \
-   --save_path ${SAVE_DIR}/checkpoint/model/llama3.1-8b-sft \
-   --ckpt_path ${SAVE_DIR}//checkpoint/opt/llama3.1-8b-sft \
+   --save_path ${SAVE_DIR}/checkpoint/model/llama3.1-8b-tool-sft \
+   --ckpt_path ${SAVE_DIR}//checkpoint/opt/llama3.1-8b-tool-sft \
    --save_steps 50 \
-   --num_process 48 \
+   --num_process 32 \
    --logging_steps 1 \
    --eval_steps -1 \
    --zero_stage 3 \
@@ -24,6 +24,6 @@ CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 deepspeed --include localhost:0,1,2,3,4,5,6
    --flash_attn \
    --learning_rate 5e-6 \
    --gradient_checkpointing \
-   --use_tensorboard ${SAVE_DIR}/checkpoint/tensorboard/llama3.1-8b-sft/tensorboard \
+   --use_tensorboard ${SAVE_DIR}/checkpoint/tensorboard/llama3.1-8b-tool-sft/tensorboard \
    --disable_fast_tokenizer \
    --ring_attn_size 2;
