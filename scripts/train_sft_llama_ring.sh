b0727aabd92ae6b740cb80d132e25c911ee5cccd
ZetangForward <zecheng.tang@foxmail.com>
2024-10-26 02:17:42 +0800

    dsa

--- scripts/train_sft_llama_ring.sh
+++ scripts/train_sft_llama_ring.sh
@@ -2,18 +2,18 @@ export CUDA_LAUNCH_BLOCKING=1
 
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 deepspeed openrlhf/cli/train_sft_dev.py \
    --max_len 64000 \
-   --dataset /data/zecheng/lcm_stack/dataset/training_data/dev_Qwen_query_answer_gen \
+   --dataset /data/zecheng/lcm_stack/dataset/training_data/Qwen_query_answer_gen \
    --input_key instruction_str \
    --output_key pred_str \
-   --train_batch_size 8 \
+   --train_batch_size 64 \
    --micro_train_batch_size 1 \
    --lora_rank 32 \
    --apply_chat_template \
    --pretrain '/data/zecheng/hf_models/Meta-Llama-3.1-8B-Instruct' \
    --save_path './checkpoint/model/llama3.1-8b-sft/' \
    --ckpt_path './checkpoint/opt/llama3.1-8b-sft/' \
-   --save_steps 20 \
-   --num_process 32 \
+   --save_steps 15 \
+   --num_process 48 \
    --logging_steps 1 \
    --eval_steps -1 \
    --zero_stage 3 \
