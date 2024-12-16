d09ab35846c267d62d310187e6f7e0a18beec6d1
ZetangForward <zecheng.tang@foxmail.com>
2024-12-16 23:52:00 +0800

    dsa

--- reetrievalheaddetect/analysis/test2.py
+++ reetrievalheaddetect/analysis/test2.py
@@ -13,109 +13,108 @@ from transformers.cache_utils import Cache
 from transformers.models.llama.modeling_llama import apply_rotary_pos_emb, repeat_kv
 from baukit import Trace
 import numpy as np
+from transformers import PreTrainedModel
 import ipdb
 # os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3"
 
 
-class AttentionAdapterBase(nn.Module):
-    def __init__(self, *args, **kwargs):
-        super().__init__()
-        self.use_flag = True
 
-    def forward(self, attn_weights):
-        if self.use_flag:
-            return self._forward(attn_weights)
+
+
+
+def hack_attn_v2(
+        self,
+        hidden_states: torch.Tensor,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_value: Optional[Cache] = None,
+        output_attentions: bool = False,
+        use_cache: bool = False,
+        cache_position: Optional[torch.LongTensor] = None,
+        position_embeddings: Optional[Tuple[torch.Tensor, torch.Tensor]] = None,  # will become mandatory in v4.46
+        attention_adapter=None,
+        **kwargs,
+    ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
+        if output_attentions:
+            # TODO: Improve this warning with e.g. `model.config.attn_implementation = "manual"` once this is implemented.
+            logger.warning_once(
+                "LlamaModel is using LlamaSdpaAttention, but `torch.nn.functional.scaled_dot_product_attention` does not support `output_attentions=True`. Falling back to the manual attention implementation, "
+                'but specifying the manual implementation will be required from Transformers version v5.0.0 onwards. This warning can be removed using the argument `attn_implementation="eager"` when loading the model.'
+            )
+            return super().forward(
+                hidden_states=hidden_states,
+                attention_mask=attention_mask,
+                position_ids=position_ids,
+                past_key_value=past_key_value,
+                output_attentions=output_attentions,
+                use_cache=use_cache,
+                cache_position=cache_position,
+                position_embeddings=position_embeddings,
+            )
+
+        bsz, q_len, _ = hidden_states.size()
+
+        query_states = self.q_proj(hidden_states)
+        key_states = self.k_proj(hidden_states)
+        value_states = self.v_proj(hidden_states)
+
+        query_states = query_states.view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
+        key_states = key_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
+        value_states = value_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
+
+        if position_embeddings is None:
+            logger.warning_once(
+                "The attention layers in this model are transitioning from computing the RoPE embeddings internally "
+                "through `position_ids` (2D tensor with the indexes of the tokens), to using externally computed "
+                "`position_embeddings` (Tuple of tensors, containing cos and sin). In v4.46 `position_ids` will be "
+                "removed and `position_embeddings` will be mandatory."
+            )
+            cos, sin = self.rotary_emb(value_states, position_ids)
         else:
-            return attn_weights
+            cos, sin = position_embeddings
+        query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin)
 
-    def _forward(self, attn_weights):
-        raise NotImplementedError
+        if past_key_value is not None:
+            # sin and cos are specific to RoPE models; cache_position needed for the static cache
+            cache_kwargs = {"sin": sin, "cos": cos, "cache_position": cache_position}
+            key_states, value_states = past_key_value.update(key_states, value_states, self.layer_idx, cache_kwargs)
 
-    def register_input_ids(self, input_ids: torch.Tensor):
-        self.input_ids = input_ids
+        key_states = repeat_kv(key_states, self.num_key_value_groups)
+        value_states = repeat_kv(value_states, self.num_key_value_groups)
 
-class AttentionAdapter(AttentionAdapterBase):
-    def __init__(self) -> None:
-        super().__init__()
-        self.params = None
+        causal_mask = attention_mask
+        if attention_mask is not None:
+            causal_mask = causal_mask[:, :, :, : key_states.shape[-2]]
 
-    def _forward(self, attn_weights):
-        if self.params is None:
-            self.params = torch.ones_like(attn_weights, requires_grad=True)
-        else:
-            self.params.data = torch.ones_like(attn_weights)
-        return attn_weights * self.params
+        # SDPA with memory-efficient backend is currently (torch==2.1.2) bugged with non-contiguous inputs with custom attn_mask,
+        # Reference: https://github.com/pytorch/pytorch/issues/112577.
+        if query_states.device.type == "cuda" and causal_mask is not None:
+            query_states = query_states.contiguous()
+            key_states = key_states.contiguous()
+            value_states = value_states.contiguous()
 
-    @property
-    def grad(self):
-        return self.params.grad
+        # We dispatch to SDPA's Flash Attention or Efficient kernels via this `is_causal` if statement instead of an inline conditional assignment
+        # in SDPA to support both torch.compile's dynamic shapes and full graph options. An inline conditional prevents dynamic shapes from compiling.
+        is_causal = True if causal_mask is None and q_len > 1 else False
 
-    def zero_grad(self, set_to_none: bool = False) -> None:
-        if self.params.grad is not None:
-            if set_to_none:
-                self.params.grad = None
-            else:
-                self.params.grad = torch.zeros_like(self.params.grad)
+        attn_output = torch.nn.functional.scaled_dot_product_attention(
+            query_states,
+            key_states,
+            value_states,
+            attn_mask=causal_mask,
+            dropout_p=self.attention_dropout if self.training else 0.0,
+            is_causal=is_causal,
+        )
 
-def manager_decoractor(manager):
-    def model_forward_decorator(fn):
-        @wraps(fn)
-        def wrapper(*args, **kwargs):
-            input_ids = kwargs.get('input_ids', None)
-            if input_ids is None:
-                input_ids = args[0]
-            manager.register_input_ids(input_ids)
-            return fn(*args, **kwargs)
+        if attention_adapter is not None:  # pass attention weights to adapter
+            attn_output = attention_adapter(attn_output)
 
-        return wrapper
+        attn_output = attn_output.transpose(1, 2).contiguous()
+        attn_output = attn_output.view(bsz, q_len, -1)
+        
+        attn_output = self.o_proj(attn_output)
 
-    return model_forward_decorator
-
-
-
-class AttentionerManagerBase:
-    def __init__(self, model):
-        self.model = model
-        self.attention_adapters = self.register_attentioner_to_model()
-        self.model.forward = manager_decoractor(self)(self.model.forward)
-
-    @property
-    def input_ids(self):
-        return self._input_ids
-
-    @input_ids.setter
-    def input_ids(self, input_ids):
-        self._input_ids = input_ids
-        for attention_adapter in self.attention_adapters:
-            attention_adapter.register_input_ids(input_ids)
-
-    def register_input_ids(self, input_ids):
-        self.input_ids = input_ids
-
-    def register_attentioner_to_model(self):
-        raise NotImplementedError
-
-    def zero_grad(self,set_to_none=True):
-        if set_to_none:
-            for attention_adapter in self.attention_adapters:
-                attention_adapter.params = None
-        else:
-            for attention_adapter in self.attention_adapters:
-                attention_adapter.zero_grad(set_to_none=True)
-
-    def grad_process(self, grad,use_abs = True):
-        assert len(grad.shape) == 4
-        grad = grad.sum(1)
-        if use_abs:
-            grad = abs(grad)
-        return grad
-
-    def grad(self,*args,**kwargs):
-        grads = []
-        for attention_adapter in self.attention_adapters:
-            grads.append(self.grad_process(attention_adapter.params.grad,*args,**kwargs))
-        return grads
-
+        return attn_output, None, past_key_value
 
 def hack_attn(
     self,
@@ -184,7 +183,8 @@ def hack_attn(
         attn_weights = attn_weights + causal_mask
 
     # upcast attention to fp32
-    attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
+    # attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
+    attn_weights = nn.functional.softmax(attn_weights, dim=-1).to(query_states.dtype) # FIXME: use bf16 to save memory
 
     if attention_adapter is not None:  # pass attention weights to adapter
         attn_weights = attention_adapter(attn_weights)
@@ -216,18 +216,116 @@ def hack_attn(
 
 
 
-class AttentionerManager(AttentionerManagerBase):
-    def __init__(self, model):
-        super().__init__(model)
+class AttentionAdapterBase(nn.Module):
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        self.use_flag = True
+
+    def forward(self, attn_weights):
+        if self.use_flag:
+            return self._forward(attn_weights)
+        else:
+            return attn_weights
+
+    def _forward(self, attn_weights):
+        raise NotImplementedError
+
+    def register_input_ids(self, input_ids: torch.Tensor):
+        self.input_ids = input_ids
+
+
+class AttentionAdapter(AttentionAdapterBase):
+    def __init__(self) -> None:
+        super().__init__()
+        self.params = None
+
+    def _forward(self, attn_weights):
+        if self.params is None:
+            self.params = torch.ones_like(attn_weights, requires_grad=True)
+        else:
+            self.params.data = torch.ones_like(attn_weights)
+        return attn_weights * self.params
+
+    @property
+    def grad(self):
+        return self.params.grad
+
+    def zero_grad(self, set_to_none: bool = False) -> None:
+        if self.params.grad is not None:
+            if set_to_none:
+                self.params.grad = None
+            else:
+                self.params.grad = torch.zeros_like(self.params.grad)
+
+def manager_decoractor(manager):
+    def model_forward_decorator(fn):
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            input_ids = kwargs.get('input_ids', None)
+            if input_ids is None:
+                input_ids = args[0]
+            manager.register_input_ids(input_ids)
+            return fn(*args, **kwargs)
+
+        return wrapper
+
+    return model_forward_decorator
+
+
+
+class AttentionerManagerBase:
+    def __init__(self, model, attention_adapters: List[AttentionAdapterBase]):
+        self.model = model
+        self.attention_adapters = attention_adapters
+        self.model.forward = manager_decoractor(self)(self.model.forward)
+        self.register_attentioner_to_model()
+
+    @property
+    def input_ids(self):
+        return self._input_ids
+
+    @input_ids.setter
+    def input_ids(self, input_ids):
+        self._input_ids = input_ids
+        for attention_adapter in self.attention_adapters:
+            attention_adapter.register_input_ids(input_ids)
+
+    def register_input_ids(self, input_ids):
+        self.input_ids = input_ids
 
     def register_attentioner_to_model(self):
-        attention_adapters = []
+        raise NotImplementedError
+
+    def zero_grad(self,set_to_none=True):
+        if set_to_none:
+            for attention_adapter in self.attention_adapters:
+                attention_adapter.params = None
+        else:
+            for attention_adapter in self.attention_adapters:
+                attention_adapter.zero_grad(set_to_none=True)
+
+    def grad_process(self, grad,use_abs = True):
+        assert len(grad.shape) == 4
+        grad = grad.sum(1)
+        if use_abs:
+            grad = abs(grad)
+        return grad
+
+    def grad(self,*args,**kwargs):
+        grads = []
+        for attention_adapter in self.attention_adapters:
+            grads.append(self.grad_process(attention_adapter.params.grad,*args,**kwargs))
+        return grads
+
+
+class AttentionerManager(AttentionerManagerBase):
+    def __init__(self, model: PreTrainedModel, attention_adapters: List[AttentionAdapterBase]):
+        super().__init__(model, attention_adapters)
+
+    def register_attentioner_to_model(self):
         for i, layer in enumerate(self.model.model.layers):
-            attention_adapter = AttentionAdapter()
             # layer.config = self.model.config
-            layer.self_attn.forward = partial(hack_attn, layer.self_attn, attention_adapter=attention_adapter)
-            attention_adapters.append(attention_adapter)
-        return attention_adapters
+            layer.self_attn.forward = partial(hack_attn, layer.self_attn, attention_adapter=self.attention_adapters[i])
 
 
 def get_proportion(saliency, class_poss, final_poss):
@@ -256,8 +354,9 @@ def test_model_with_attention_adapter(model, input, golden):
     attentionermanger = AttentionerManager(model)
     attentionermanger.zero_grad()
     output = model(input)
-    import ipdb; ipdb.set_trace()
-    loss = F.cross_entropy(output['logits'], golden)
+    logits = output['logits'][:, 1:, :]
+    labels = golden[:, :-1]
+    loss = F.cross_entropy(logits.view(-1, logits.size(-1)), labels.view(-1))
     loss.backward()
 
     pros = []
@@ -271,9 +370,25 @@ def test_model_with_attention_adapter(model, input, golden):
     # pros_list.append(pros)
 
 
-if __name__ == "__main__":
+def find_multi_needle_idx(evidences, tokenizer, needle):
+    all_evi_pos = []
+    for i, evi in enumerate(needle):
+        needle_ids = tokenizer(evi, add_special_tokens=False)["input_ids"]
+        print(f"evidence {i} --> {tokenizer.decode(needle_ids, skip_special_tokens=False)}")
+        span_len = len(needle_ids)
+        for j in range(len(evidences)):
+            token_span = evidences[j : j + span_len]
+            span_ids = set(token_span.tolist())
+            overlap = float(len(span_ids.intersection(set(needle_ids)))) / len(set(needle_ids))
+            if(overlap > 0.8):
+                all_evi_pos.append((j + 1, j + span_len))
+                print(f"find evidence {i} at --> {(j + 1, j + span_len)} --> {tokenizer.decode(evidences[j + 1: j + span_len], skip_special_tokens=False)}")
+                break
+    return all_evi_pos
 
-    model = LlamaForCausalLM.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct", device_map='balanced_low_0', torch_dtype=torch.bfloat16, attn_implementation="eager")
+
+if __name__ == "__main__":
+    model = LlamaForCausalLM.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct", device_map='auto', torch_dtype=torch.bfloat16, attn_implementation="eager")
     tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct")
     data = auto_read_data("../haystack_for_detect/reasoning_needle.jsonl")
 
@@ -295,7 +410,7 @@ if __name__ == "__main__":
     # 初始化采样器
     haystack = datasets.load_dataset("/mnt/petrelfs/tangzecheng/local_data/pg19-test", split="test")
     noise_sampler_test = SentenceSampler(haystack, tokenizer=tokenizer, shuffle=False, random_seed=None)
-    background_text = noise_sampler_test.get_sample(15500)
+    background_text = noise_sampler_test.get_sample(4800)
     disturb_tok_needles = [i for i in needle if i not in evidence]
     disturb_pos = np.random.choice(len(background_text)+1, len(disturb_tok_needles))
     depth_percent = [0.1, 0.2, 0.3]
