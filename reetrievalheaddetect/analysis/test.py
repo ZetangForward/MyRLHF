d7b1797695ff7ff126b046404d6634bcdfb5d4bd
ZetangForward <zecheng.tang@foxmail.com>
2024-12-19 00:06:39 +0800

    dsa

--- reetrievalheaddetect/analysis/test.py
+++ reetrievalheaddetect/analysis/test.py
@@ -173,6 +173,64 @@ def hack_attn_qwen(
     return attn_output, attn_weights, past_key_value
 
 
+def hack_attn_mistral(
+    self,
+    hidden_states: torch.Tensor,
+    attention_mask: Optional[torch.Tensor] = None,
+    position_ids: Optional[torch.LongTensor] = None,
+    past_key_value: Optional[Cache] = None,
+    output_attentions: bool = False,
+    use_cache: bool = False,
+    cache_position: Optional[torch.LongTensor] = None,
+) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
+    bsz, q_len, _ = hidden_states.size()
+
+    query_states = self.q_proj(hidden_states)
+    key_states = self.k_proj(hidden_states)
+    value_states = self.v_proj(hidden_states)
+
+    query_states = query_states.view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
+    key_states = key_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
+    value_states = value_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
+
+    cos, sin = self.rotary_emb(value_states, position_ids)
+    query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin)
+
+    if past_key_value is not None:
+        # sin and cos are specific to RoPE models; cache_position needed for the static cache
+        cache_kwargs = {"sin": sin, "cos": cos, "cache_position": cache_position}
+        key_states, value_states = past_key_value.update(key_states, value_states, self.layer_idx, cache_kwargs)
+
+    key_states = repeat_kv(key_states, self.num_key_value_groups)
+    value_states = repeat_kv(value_states, self.num_key_value_groups)
+
+    attn_weights = torch.matmul(query_states, key_states.transpose(2, 3)) / math.sqrt(self.head_dim)
+
+    if attention_mask is not None:  # no matter the length, we just slice it
+        causal_mask = attention_mask[:, :, :, : key_states.shape[-2]]
+        attn_weights = attn_weights + causal_mask
+
+    # upcast attention to fp32
+    attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
+    attn_weights = nn.functional.dropout(attn_weights, p=self.attention_dropout, training=self.training)
+    attn_output = torch.matmul(attn_weights, value_states)
+
+    if attn_output.size() != (bsz, self.num_heads, q_len, self.head_dim):
+        raise ValueError(
+            f"`attn_output` should be of size {(bsz, self.num_heads, q_len, self.head_dim)}, but is"
+            f" {attn_output.size()}"
+        )
+
+    attn_output = attn_output.transpose(1, 2).contiguous()
+
+    attn_output = attn_output.view(bsz, q_len, -1)
+    attn_output = self.o_proj(attn_output)
+
+    if not output_attentions:
+        attn_weights = None
+
+    return attn_output, attn_weights, past_key_value
+
 def hack_attn_llama(
     self,
     hidden_states: torch.Tensor,
@@ -374,6 +432,7 @@ class AttentionerManagerBase:
         return grads
 
 
+
 # class AttentionerManager(AttentionerManagerBase):
 #     def __init__(self, model: PreTrainedModel, attention_adapters: List[AttentionAdapterBase]):
 #         super().__init__(model, attention_adapters)
@@ -396,6 +455,10 @@ class AttentionerManager(AttentionerManagerBase):
                 layer.self_attn.forward = partial(hack_attn_llama, layer.self_attn, attention_adapter=attention_adapter)
             elif "qwen" in self.model_name.lower():
                 layer.self_attn.forward = partial(hack_attn_qwen, layer.self_attn, attention_adapter=attention_adapter)
+            elif "phi" in self.model_name.lower():
+                layer.self_attn.forward = partial(hack_attn_phi, layer.self_attn, attention_adapter=attention_adapter)
+            elif "mistral" in self.model_name.lower():
+                layer.self_attn.forward = partial(hack_attn_mistral, layer.self_attn, attention_adapter=attention_adapter)
             else:
                 raise NotImplementedError(f"{self.model_name} not supported")
             attention_adapters.append(attention_adapter)
@@ -436,7 +499,7 @@ def calculate_portions(saliency, evi_poss: List[Tuple[int, int]], attack_pos: Li
         proportion1 += saliency[target_poss, np.array(range(span_idx[0], span_idx[1]))].sum()
         evidence_length += span_idx[1] - span_idx[0]
         # evidence proportions
-        evidence_proportions.append(saliency[target_poss, np.array(range(span_idx[0], span_idx[1]))].sum() // (span_idx[1] - span_idx[0]))
+        evidence_proportions.append(saliency[target_poss, np.array(range(span_idx[0], span_idx[1]))].sum() / (span_idx[1] - span_idx[0]))
 
     # proportion2: all context -> target token
     proportion2 = saliency[target_poss, :].sum()
