17d2561d8612f94d4dd38d5b6a9e91cdbcf5ee1b
ZetangForward <zecheng.tang@foxmail.com>
2024-12-29 00:02:11 +0800

    das

--- build_data/preprocess_data/multi_hop/cal_model_inference.py
+++ build_data/preprocess_data/multi_hop/cal_model_inference.py
@@ -14,45 +14,7 @@ import subprocess
 import itertools
 import random
 from multiprocessing import Pool, cpu_count
-
-def get_gpu_memory():
-    """
-    获取所有GPU的显存使用情况
-    返回: [(GPU ID, 已用显存, 总显存), ...]
-    """
-    try:
-        output = subprocess.check_output(
-            ['nvidia-smi', '--query-gpu=index,memory.used,memory.total', '--format=csv,nounits,noheader']
-        )
-        lines = output.decode().strip().split('\n')
-        return [tuple(map(int, line.split(','))) for line in lines]
-    except subprocess.CalledProcessError as e:
-        print(f"Error occurred while running nvidia-smi: {e}")
-        return []
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-        return []
-
-
-def get_free_gpu(threshold=300):  # threshold单位为MB
-    """
-    获取空闲的GPU ID
-    threshold: 小于此显存使用量(MB)的GPU被认为是空闲的
-    """
-    gpu_memory = get_gpu_memory()
-    if not gpu_memory:
-        print("No GPUs available.")
-        return []
-    empty_gpus = []
-    for gpu_id, memory_used, memory_total in gpu_memory:
-        if memory_used < threshold:
-            empty_gpus.append(gpu_id)
-            print(f"GPU {gpu_id} is available: {memory_used}MB/{memory_total}MB used")
-        else:
-            print(f"GPU {gpu_id} is busy: {memory_used}MB/{memory_total}MB used")
-    return empty_gpus
-
-
+from tqdm import tqdm
 
 def worker(gpu_ids: str, prompts_chunk, model_path, model_args, inference_args, return_list):
     os.environ['CUDA_VISIBLE_DEVICES'] = gpu_ids
@@ -119,8 +81,7 @@ def process_data_item(args):
     meta_data.pop('instruction_format')
     meta_data.pop('answer')
     return {
-        "prompt": prompt,
-        "ori_prompt": full_evi_prompt,
+        "prompt": full_evi_prompt,
         "message": input_data,
         "answer": answer,
         "meta_data": meta_data, 
@@ -169,78 +130,79 @@ class Args:
     def h20(self):
         self.model_path = "/data/zecheng/hf_models/Meta-Llama-3.1-8B-Instruct"
         self.dataset_path = "/data/zecheng/data/processed_multi_hop/filter_en"
-        self.out_file_path = "/data/zecheng/data/processed_multi_hop/random_drop"
+        self.out_file_path = "/data/zecheng/data/processed_multi_hop/random_drop_fix"
 
     def pjlab(self):
         self.model_path = "meta-llama/Meta-Llama-3.1-8B-Instruct"
         self.dataset_path = "/mnt/petrelfs/tangzecheng/local_data/processed_multi_hop/filter_en"
-        self.out_file_path = "/mnt/petrelfs/tangzecheng/local_data/processed_multi_hop/random_drop"
+        self.out_file_path = "/mnt/petrelfs/tangzecheng/local_data/processed_multi_hop/random_drop_fix"
 
 
 def main(args):
-    tokenizer = AutoTokenizer.from_pretrained(args.model_path)
-    all_file_names = auto_read_dir(args.dataset_path)
-    content = []
-    for file_name in all_file_names:
-        content.extend(auto_read_data(os.path.join(args.dataset_path, file_name)))
-    logger.info(f"length of content {len(content)}, begin to preprocess")
-    # content = content[:24]  # FIXME: debug
-    input_queries = process_data(content, tokenizer, drop_num=args.drop_num, num_workers=64)
-    
-    chunk_num = args.num_gpus // args.tp_size
-    chunk_size = (len(input_queries) + chunk_num - 1) // chunk_num
-    prompts_chunks = [input_queries[i*chunk_size:(i+1)*chunk_size] for i in range(chunk_num)]
+    for drop_num in [1, 2, 3]:
+        tokenizer = AutoTokenizer.from_pretrained(args.model_path)
+        all_file_names = auto_read_dir(args.dataset_path)
+        content = []
+        for file_name in all_file_names:
+            content.extend(auto_read_data(os.path.join(args.dataset_path, file_name)))
+        logger.info(f"length of content {len(content)}, begin to preprocess")
+        # content = content[:24]  # FIXME: debug
+        input_queries = process_data(content, tokenizer, drop_num=drop_num, num_workers=16)
+        
+        chunk_num = args.num_gpus // args.tp_size
+        chunk_size = (len(input_queries) + chunk_num - 1) // chunk_num
+        prompts_chunks = [input_queries[i*chunk_size:(i+1)*chunk_size] for i in range(chunk_num)]
+        all_length = sum([len(chunk) for chunk in prompts_chunks])
+        logger.info(f"length of input_queries {all_length}")
 
-    manager = mp.Manager()
-    return_list = manager.list()
-    processes = []
+        manager = mp.Manager()
+        return_list = manager.list()
+        processes = []
 
-    # avail_gpu_ids = get_empty_gpus()
-    avail_gpu_ids = list(range(args.num_gpus))
-    avail_gpu_ids = avail_gpu_ids[:len(avail_gpu_ids)//args.tp_size * args.tp_size]
-    if len(avail_gpu_ids) == 0:
-        logger.error("No available GPUs.")
-        exit(1)
-    
-    # construct gpu_ids list
-    if args.tp_size == 1:
-        gpu_id_lst = [str(i) for i in range(args.num_gpus)]
-    else:
-        gpu_id_lst = []
-        for j in range(0, len(avail_gpu_ids), args.tp_size):
-            tmp = [avail_gpu_ids[i + j] for i in range(args.tp_size)]
-            gpu_id_lst.append(", ".join([str(i) for i in tmp]))
+        # avail_gpu_ids = get_empty_gpus()
+        avail_gpu_ids = list(range(args.num_gpus))
+        avail_gpu_ids = avail_gpu_ids[:len(avail_gpu_ids)//args.tp_size * args.tp_size]
+        if len(avail_gpu_ids) == 0:
+            logger.error("No available GPUs.")
+            exit(1)
+        
+        # construct gpu_ids list
+        if args.tp_size == 1:
+            gpu_id_lst = [str(i) for i in range(args.num_gpus)]
+        else:
+            gpu_id_lst = []
+            for j in range(0, len(avail_gpu_ids), args.tp_size):
+                tmp = [avail_gpu_ids[i + j] for i in range(args.tp_size)]
+                gpu_id_lst.append(", ".join([str(i) for i in tmp]))
 
-    # worker(gpu_id_lst[0], prompts_chunks[0], args.model_path, args.model_args, args.inference_args, return_list)  # FIXME: Debug
-    
-    # 使用 tqdm 显示总进度
-    logger.info(f"Start to generate")
-    for chunk_id, gpu_ids in enumerate(gpu_id_lst):
-        p = mp.Process(target=worker, args=(gpu_ids, prompts_chunks[chunk_id], args.model_path, args.model_args, args.inference_args, return_list))
-        p.start()
-        processes.append(p)
+        # worker(gpu_id_lst[0], prompts_chunks[0], args.model_path, args.model_args, args.inference_args, return_list)  # FIXME: Debug
+        
+        # 使用 tqdm 显示总进度
+        logger.info(f"Start to generate")
+        for chunk_id, gpu_ids in enumerate(gpu_id_lst):
+            p = mp.Process(target=worker, args=(gpu_ids, prompts_chunks[chunk_id], args.model_path, args.model_args, args.inference_args, return_list))
+            p.start()
+            processes.append(p)
 
-    for p in processes:
-        p.join()
-    
-    # 保存生成结果
-    logger.info('Have collected ', len(return_list), 'samples, begin to save ...')
-    normal_list = list(return_list)
-    auto_mkdir(args.out_file_path)
-    auto_save_data(normal_list, os.path.join(args.out_file_path, f"inference_drop_{args.drop_num}.pkl"))
+        for p in processes:
+            p.join()
+        
+        # 保存生成结果
+        logger.info('Have collected ', len(return_list), 'samples, begin to save ...')
+        normal_list = list(return_list)
+        auto_mkdir(args.out_file_path)
+        auto_save_data(normal_list, os.path.join(args.out_file_path, f"inference_drop_{drop_num}.pkl"))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--drop_num', type=int, default=1,  help="randomly dropped evidence number")
     parser.add_argument('--model_path', type=str, default="meta-llama/Meta-Llama-3.1-8B-Instruct",  help="model name or path")
     parser.add_argument('--out_file_path', type=str, default="/data/zecheng/data/processed_multi_hop/random_drop",  help="save path for inference results")
-    parser.add_argument('--num_gpus', type=int, default="/data/zecheng/data/processed_multi_hop/random_drop",  help="save path for inference results")
+    parser.add_argument('--num_gpus', type=int, default=8,  help="number of gpus")
     
     extra_args = parser.parse_args()
 
     args = Args("pjlab")
-    args.drop_num = extra_args.drop_num
     args.model_path = extra_args.model_path
     args.out_file_path = extra_args.out_file_path
     args.num_gpus = extra_args.num_gpus
