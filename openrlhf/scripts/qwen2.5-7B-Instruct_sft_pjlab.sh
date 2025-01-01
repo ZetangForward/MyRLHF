b9c189166fed65293f879361499d893850d8f10d
ZetangForward <zecheng.tang@foxmail.com>
2025-01-01 12:09:37 +0800

    dsa

--- openrlhf/scripts/qwen2.5-7B-Instruct_sft_pjlab.sh
+++ openrlhf/scripts/qwen2.5-7B-Instruct_sft_pjlab.sh
@@ -5,8 +5,8 @@ SAVE_DIR='/mnt/petrelfs/tangzecheng/local_ckpt'
 
 deepspeed --include localhost:0,1,2,3,4,5,6,7 cli/train_sft.py \
    --max_len 96000 \
-   --dataset '/mnt/petrelfs/tangzecheng/local_data/processed_multi_hop/random_drop/train_qwen_data/merge_v1' \
-   --train_batch_size 64 \
+   --dataset '/mnt/petrelfs/tangzecheng/local_data/processed_multi_hop/random_drop/train_llama_data/merge_v1_w_clues' \
+   --train_batch_size 32 \
    --micro_train_batch_size 1 \
    --lora_rank 32 \
    --apply_chat_template \
@@ -30,4 +30,5 @@ deepspeed --include localhost:0,1,2,3,4,5,6,7 cli/train_sft.py \
    --use_wandb 'f81f2a236e712350a0ec153e02f43d1366c856a5' \
    --wandb_project 'merge_v1_fix' \
    --wandb_run_name 'Qwen2.5-7B-Instruct-sft' \
-   --ring_attn_size 2;
+   --ring_attn_size 4 \
+   --ring_head_stride 2;
